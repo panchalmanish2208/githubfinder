c5430d8a97a2a5165d6ade44773cbc180389690d
Manish Panchal <panchalmanish2208@gmail.com>
2024-05-12 11:27:02 +0530

    first-commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,19 @@
+function About() {
+    return (
+     <>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+       A React app to search GitHub profiles and see profile details. This project is created by
+       <strong>
+        <a href="https://www.linkedin.com/in/panchalmanish2208/"> Manish Panchal</a>
+       </strong>
+       .
+      </p>
+      <p className="text-lg text-gray-400">
+       Version <span className="text-gray-400">1.0.0</span>
+      </p>
+     </>
+    );
+   }
+
+export default About;
