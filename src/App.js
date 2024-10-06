85b077cb0482e2f8d4cb6812b9ddf1b6213b643d
panchalmanish2208 <panchalmanish2208@gmail.com>
2024-10-06 22:54:01 +0530

    update: Add user component and spinner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom"; 
-
+import { HashRouter as Router, Route, Routes} from "react-router-dom"; 
+//import { BrowserRouter as Router, Route, Routes} from "react-router-dom"; 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Home from './pages/Home';
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
+import UserResults from "../components/users/UserResults";
+
 function Home(){
     return (
-        <div>
-            <h1 className="text-6xl">
-                Welcome
-            </h1>
-        </div>
+        // <div>
+        //     <h1 className="text-6xl">
+        //         Welcome
+        //     </h1>
+        // </div>4
+        <>
+        <UserResults />
+        </>
     )
 }
 
